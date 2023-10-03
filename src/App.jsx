c721b0a574fba847423752f64c0c2e8bbacbb77b
Grig T <tonikyang@gmail.com>
2023-10-03 06:14:04 +0400

    Vite/TS React Starter empty project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+import { Event } from './Event/Event';
+
+export default function App() {
+  return (
+    <div>
+      <Event />
+    </div>
+  );
+}
---
+++ src/Event/Event.jsx
@@ -0,0 +1,247 @@
+/* Code generated with AutoHTML Plugin for Figma */
+import './Event.css';
+
+export const Event = ({ ...props }) => {
+  return (
+    <div className='event'>
+      <div className='date-and-time'>
+        <div className='_26-09'>26.09 </div>
+        <div className='_22-45'>22:45 </div>
+      </div>
+      <div className='event2'>
+        <div className='league-flag'>
+          <div className='flag'>
+            <svg className='bg' width='20' height='20'></svg>
+            <div className='mask-layer'></div>
+            <div className='_51'>
+              <svg className='rectangle-11411' width='16' height='12' viewBox='0 0 16 12' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                <path d='M16 0.667969H0V11.335H16V0.667969Z' fill='#FFDA44' />
+              </svg>
+
+              <svg className='group-28120' width='16' height='12' viewBox='0 0 16 12' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                <path d='M16 0.667969H0V4.22297H16V0.667969Z' fill='#D80027' />
+                <path d='M16 7.77905H0V11.3341H16V7.77905Z' fill='#D80027' />
+              </svg>
+            </div>
+          </div>
+          <div className='spain-primera-division'>Spain. Primera Division </div>
+        </div>
+        <div className='event3'>
+          <div className='sport-icons'>
+            <svg className='bg2' width='20' height='20'></svg>
+
+            <svg className='_1' width='16' height='16' viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'>
+              <path
+                d='M15.1832 7.00835L14.5415 6.23335L14.7498 5.35001C14.8082 5.15835 14.9748 5.16668 15.0248 5.30002C15.2332 5.83335 15.3748 6.38335 15.4582 6.94168C15.4748 7.10002 15.2915 7.15002 15.1832 7.00835Z'
+                fill='white'
+                fillOpacity='0.4'
+              />
+              <path
+                d='M13.1664 13.4501C12.7164 13.8834 12.2081 14.2501 11.6581 14.5584C11.4414 14.6751 11.1914 14.5501 11.3497 14.2668C11.4831 14.0334 11.8831 13.3418 11.8831 13.3418C11.8831 13.3418 12.6997 13.2168 12.9664 13.1668C13.2164 13.1168 13.2914 13.3251 13.1664 13.4501Z'
+                fill='white'
+                fillOpacity='0.4'
+              />
+              <path
+                d='M4.53318 14.65C3.91651 14.3333 3.34151 13.925 2.83318 13.45C2.73318 13.3583 2.69151 13.1167 3.14151 13.1833C3.39985 13.225 4.14985 13.35 4.14985 13.35C4.14985 13.35 4.65818 14.1333 4.81651 14.3833C5.00818 14.675 4.74985 14.7667 4.53318 14.65Z'
+                fill='white'
+                fillOpacity='0.4'
+              />
+              <path
+                d='M11.0497 12.9666L9.89967 14.975C9.77467 15.1833 9.57467 15.3417 9.33301 15.3833C8.52467 15.5333 7.68301 15.5417 6.86634 15.4167C6.63301 15.3833 6.42467 15.2333 6.29967 15.0417L4.95801 12.9583L6.29134 11.1917H9.69134L11.0497 12.9666Z'
+                fill='white'
+                fillOpacity='0.4'
+              />
+              <path d='M10.25 7.61686L9.56667 10.2919H6.425L5.75 7.61686L8 5.8252L10.25 7.61686Z' fill='white' fillOpacity='0.4' />
+              <path
+                d='M5.59141 10.6417L4.22475 12.45L2.05808 12.1084C1.80808 12.0667 1.59141 11.925 1.46641 11.7084C1.01641 10.9167 0.699745 10.0334 0.566412 9.13337C0.533079 8.90003 0.608079 8.65837 0.758079 8.47503L2.10808 6.8667L4.88308 7.7917L5.59141 10.6417Z'
+                fill='white'
+                fillOpacity='0.4'
+              />
+              <path
+                d='M1.44986 6.2418L0.858198 6.95846C0.691532 7.15846 0.524865 7.0668 0.549865 6.90846C0.624865 6.38346 0.758198 5.8668 0.949865 5.3668C1.0082 5.20846 1.21653 5.1668 1.2582 5.40013L1.44986 6.2418Z'
+                fill='white'
+                fillOpacity='0.4'
+              />
+              <path
+                d='M7.54977 2.27518V5.04185L5.1581 6.95018L2.32476 6.00851L1.8831 4.13351C1.82476 3.89185 1.87476 3.63351 2.02476 3.44185C2.69143 2.58351 3.5331 1.87518 4.49143 1.36685C4.69976 1.25018 4.9581 1.25018 5.17476 1.33351L7.54977 2.27518Z'
+                fill='white'
+                fillOpacity='0.4'
+              />
+              <path
+                d='M9.21676 1.00008L8.0001 1.49174L6.8501 1.04174C6.63343 0.966745 6.63343 0.625078 6.9001 0.583411C7.64176 0.475078 8.39176 0.475078 9.13343 0.591744C9.38343 0.625078 9.40843 0.925078 9.21676 1.00008Z'
+                fill='white'
+                fillOpacity='0.4'
+              />
+              <path
+                d='M14.108 4.13353L13.6664 6.00853L10.8414 6.9502L8.44971 5.04186V2.2752L10.808 1.33353C11.033 1.24186 11.283 1.2502 11.4997 1.35853C12.4664 1.86686 13.3164 2.56686 13.9747 3.43353C14.1164 3.63353 14.1664 3.89186 14.108 4.13353Z'
+                fill='white'
+                fillOpacity='0.4'
+              />
+              <path
+                d='M15.4332 9.14167C15.2915 10.0417 14.9915 10.9167 14.5332 11.7167C14.4082 11.9333 14.1832 12.075 13.9415 12.1083L11.7832 12.4417L10.4165 10.6417L11.1332 7.8L13.8998 6.875L15.2498 8.48333C15.3915 8.66667 15.4665 8.90833 15.4332 9.14167Z'
+                fill='white'
+                fillOpacity='0.4'
+              />
+            </svg>
+          </div>
+          <div className='real-madrid-barcelona'>
+            <span>
+              <span className='real-madrid-barcelona-span'>Real Madrid </span>
+              <span className='real-madrid-barcelona-span2'>-</span>
+              <span className='real-madrid-barcelona-span3'> Barcelona</span>
+            </span>{' '}
+          </div>
+        </div>
+      </div>
+      <div className='live-tv-live-info'>
+        <div className='spb-main-icons'>
+          <svg className='bg3' width='16' height='16'></svg>
+
+          <svg className='live-tv' width='16' height='12' viewBox='0 0 16 12' fill='none' xmlns='http://www.w3.org/2000/svg'>
+            <path
+              d='M15.1998 7.2H0.799805V0.8C0.799805 0.36 1.1598 0 1.5998 0H14.3998C14.8398 0 15.1998 0.36 15.1998 0.8V7.2Z'
+              fill='white'
+              fillOpacity='0.4'
+            />
+            <path
+              d='M15.1998 8H0.799805V8.4C0.799805 8.84 1.1598 9.2 1.5998 9.2H14.3998C14.8398 9.2 15.1998 8.84 15.1998 8.4V8Z'
+              fill='white'
+              fillOpacity='0.4'
+            />
+            <path d='M4 12H12V11.6C12 11.16 11.64 10.8 11.2 10.8H4.8C4.36 10.8 4 11.16 4 11.6V12Z' fill='white' fillOpacity='0.4' />
+            <path d='M8 9.19995V10.8' stroke='white' strokeOpacity='0.4' strokeWidth='2' strokeMiterlimit='10' />
+          </svg>
+        </div>
+        <div className='spb-main-icons'>
+          <svg className='bg4' width='16' height='16'></svg>
+
+          <svg className='live-info' width='16' height='12' viewBox='0 0 16 12' fill='none' xmlns='http://www.w3.org/2000/svg'>
+            <path
+              d='M13.1997 4.7999V7.1999C13.1997 7.4239 13.3757 7.5999 13.5997 7.5999H15.1997V4.3999H13.5997C13.3757 4.3999 13.1997 4.5759 13.1997 4.7999Z'
+              fill='white'
+              fillOpacity='0.4'
+            />
+            <path d='M8 8C9.10457 8 10 7.10457 10 6C10 4.89543 9.10457 4 8 4C6.89543 4 6 4.89543 6 6C6 7.10457 6.89543 8 8 8Z' fill='white' fillOpacity='0.4' />
+            <path
+              d='M2.3998 7.5999C2.6238 7.5999 2.7998 7.4239 2.7998 7.1999V4.7999C2.7998 4.5759 2.6238 4.3999 2.3998 4.3999H2.2478H0.799805V7.5999H2.2478H2.3998Z'
+              fill='white'
+              fillOpacity='0.4'
+            />
+            <path
+              d='M5.1998 5.9999C5.1998 4.5919 6.2478 3.4319 7.5998 3.2399V0.399902H1.5998C1.1598 0.399902 0.799805 0.759902 0.799805 1.1999V3.5999H2.2478H2.3998C3.0638 3.5999 3.5998 4.1359 3.5998 4.7999V7.1999C3.5998 7.8639 3.0638 8.3999 2.3998 8.3999H0.799805V10.7999C0.799805 11.2399 1.1598 11.5999 1.5998 11.5999H7.5998V8.7599C6.2478 8.5599 5.1998 7.4079 5.1998 5.9999Z'
+              fill='white'
+              fillOpacity='0.4'
+            />
+            <path
+              d='M12.3999 7.1999V4.7999C12.3999 4.1359 12.9359 3.5999 13.5999 3.5999H14.2639H15.1999V1.1999C15.1999 0.759902 14.8399 0.399902 14.3999 0.399902H8.3999V3.2399C9.7519 3.4319 10.7999 4.5919 10.7999 5.9999C10.7999 7.4079 9.7519 8.5599 8.3999 8.7599V11.5999H14.3999C14.8399 11.5999 15.1999 11.2399 15.1999 10.7999V8.3999H13.5999C12.9359 8.3999 12.3999 7.8639 12.3999 7.1999Z'
+              fill='white'
+              fillOpacity='0.4'
+            />
+          </svg>
+        </div>
+      </div>
+      <div className='result-market'>
+        <div className='win-1'>
+          <div className='size-medium-state-default'>
+            <div className='odds-buster'>
+              <div className='_1-25'>2.52 </div>
+            </div>
+          </div>
+        </div>
+        <div className='draw'>
+          <div className='size-medium-state-default'>
+            <div className='odds-buster'>
+              <div className='_1-25'>3.68 </div>
+            </div>
+          </div>
+        </div>
+        <div className='win-2'>
+          <div className='size-medium-state-default'>
+            <div className='odds-buster'>
+              <div className='_1-25'>2.20 </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='double-chance-market'>
+        <div className='_1-x'>
+          <div className='size-medium-state-default'>
+            <div className='odds-buster'>
+              <div className='_1-25'>1.67 </div>
+            </div>
+          </div>
+        </div>
+        <div className='_12'>
+          <div className='size-medium-state-default'>
+            <div className='odds-buster'>
+              <div className='_1-25'>1.28 </div>
+            </div>
+          </div>
+        </div>
+        <div className='_2-x'>
+          <div className='size-medium-state-default'>
+            <div className='odds-buster'>
+              <div className='_1-25'>1.47 </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='argument'>
+        <div className='_2-5'>2.5 </div>
+      </div>
+      <div className='double-chance-market'>
+        <div className='over'>
+          <div className='size-medium-state-default'>
+            <div className='odds-buster'>
+              <div className='_1-25'>1.67 </div>
+            </div>
+          </div>
+        </div>
+        <div className='under'>
+          <div className='size-medium-state-default'>
+            <div className='odds-buster'>
+              <div className='_1-25'>2.07 </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='more'>
+        <div className='_1200'>+1200 </div>
+      </div>
+      <div className='secondary-actions'>
+        <div className='favorite'>
+          <svg className='spb-main-icons2' width='20' height='20' viewBox='0 0 20 20' fill='none' xmlns='http://www.w3.org/2000/svg'>
+            <path
+              d='M10.0003 2L12.1703 6.34C12.4603 6.93 13.0303 7.33 13.6703 7.43L18.5003 8.12L15.0203 11.47C14.5403 11.93 14.3203 12.6 14.4403 13.25L15.2603 17.99L10.9203 15.74C10.6403 15.59 10.3203 15.51 10.0003 15.51C9.68026 15.51 9.36026 15.59 9.08026 15.74L4.74026 18L5.56026 13.26C5.67026 12.61 5.46026 11.94 4.98026 11.48L1.50026 8.13L6.33026 7.44C6.98026 7.35 7.54026 6.94 7.83026 6.35L9.99026 2.01L10.0003 2ZM9.99026 1.01C9.63026 1.01 9.28026 1.19 9.10026 1.56L6.93026 5.9C6.78026 6.19 6.50026 6.4 6.18026 6.44L1.35026 7.13C0.530262 7.25 0.200262 8.26 0.800262 8.84L4.28026 12.19C4.52026 12.42 4.63026 12.75 4.57026 13.08L3.75026 17.82C3.64026 18.47 4.15026 18.99 4.73026 18.99C4.88026 18.99 5.04026 18.95 5.19026 18.88L9.53026 16.62C9.67026 16.54 9.83026 16.51 9.99026 16.51C10.1503 16.51 10.3103 16.55 10.4503 16.62L14.7903 18.88C14.9403 18.96 15.1003 18.99 15.2503 18.99C15.8303 18.99 16.3503 18.46 16.2303 17.82L15.4103 13.08C15.3503 12.75 15.4603 12.42 15.7003 12.19L19.1803 8.84C19.7803 8.26 19.4503 7.25 18.6303 7.13L13.8003 6.44C13.4803 6.39 13.1903 6.19 13.0503 5.9L10.8903 1.55C10.7103 1.18 10.3503 1 10.0003 1L9.99026 1.01Z'
+              fill='white'
+              fillOpacity='0.6'
+            />
+          </svg>
+        </div>
+        <div className='statistics'>
+          <div className='spb-main-icons3'>
+            <svg className='bg6' width='20' height='20'></svg>
+
+            <svg className='statistics2' width='17' height='18' viewBox='0 0 17 18' fill='none' xmlns='http://www.w3.org/2000/svg'>
+              <path
+                d='M16.0005 5H14.0005C13.4505 5 13.0005 5.45 13.0005 6V17C13.0005 17.55 13.4505 18 14.0005 18H16.0005C16.5505 18 17.0005 17.55 17.0005 17V6C17.0005 5.45 16.5505 5 16.0005 5Z'
+                fill='white'
+                fillOpacity='0.6'
+              />
+              <path
+                d='M10.0005 0H8.00049C7.45049 0 7.00049 0.45 7.00049 1V17C7.00049 17.55 7.45049 18 8.00049 18H10.0005C10.5505 18 11.0005 17.55 11.0005 17V1C11.0005 0.45 10.5505 0 10.0005 0Z'
+                fill='white'
+                fillOpacity='0.6'
+              />
+              <path
+                d='M3.66699 9H1.66699C1.11699 9 0.666992 9.45 0.666992 10V17C0.666992 17.55 1.11699 18 1.66699 18H3.66699C4.21699 18 4.66699 17.55 4.66699 17V10C4.66699 9.45 4.21699 9 3.66699 9Z'
+                fill='white'
+                fillOpacity='0.6'
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
